17985142974c5da6003f6ef877f35db609625183
Chuzyh <Chuzyh@github.com>
2022-10-26 13:10:10 +0800

    hw2_code change_name

--- homework2/code/B.cpp
+++ homework2/code/B.cpp
@@ -1,6 +1,6 @@
 
 #include<bits/stdc++.h>
-#include "newton.h"
+#include "NewtonInterpolation.h"
 using namespace std;
 Newton_Interpolation f;
 int main()
--- homework2/code/C.cpp
+++ homework2/code/C.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-#include "newton.h"
+#include "NewtonInterpolation.h"
 using namespace std;
 Newton_Interpolation f;
 const double pi=acos(-1);
